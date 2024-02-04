60d86bef110c72cbd29af58660ff4dc4844580a4
Breno Ocanha <breno@dmxdesign.com.br>
2024-02-04 11:50:08 -0300

    feat: table complete

--- src/components/core/DataTable/index.tsx
+++ src/components/core/DataTable/index.tsx
@@ -11,11 +11,54 @@ interface DataTableProps {
 
 const DataTable = ({ products }: DataTableProps) => {
 
+  const metadeDosProdutos = Math.ceil(products.length / 2);
+  const primeiraMetade = products.slice(0, metadeDosProdutos);
+  const segundaMetade = products.slice(metadeDosProdutos);
+
+  const isEven = primeiraMetade.length === segundaMetade.length
+
   return (
     <>
-      {products.map((item, index) => (
-        <h1 key={index}>{item.produto}</h1>
-      ))}
+      <div className="grid grid-cols-2 gap-2 w-full">
+        <div className="w-full">
+          <table className="w-full table-auto">
+            <tbody className="flex w-full flex-col min-h-[calc(100vh-118px)] flex-grow">
+              {primeiraMetade.map((item, index) => (
+                <tr
+                  key={index}
+                  className={index % 2 === 0 ? 'bg-[#FEFEFE] flex justify-between flex-grow items-center' : 'bg-[#FCD3B7] flex justify-between flex-grow items-center'}
+                >
+                  <td className="px-4 py-2 text-xl font-bold">{item.produto}</td>
+                  <td className="px-4 py-2 w-full max-w-32 text-xl font-bold">{item.valor}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="w-full">
+          <table className="w-full table-auto">
+            <tbody className="flex w-full flex-col min-h-[calc(100vh-118px)] flex-grow">
+              {segundaMetade.map((item, index) => (
+                <tr
+                  key={index}
+                  className={index % 2 === 0 ? 'bg-[#FEFEFE] flex justify-between flex-grow items-center' : 'bg-[#FCD3B7] flex justify-between flex-grow items-center'}
+                >
+                  <td className="px-4 py-2 text-xl font-bold">{item.produto}</td>
+                  <td className="px-4 py-2 w-full max-w-32 text-xl font-bold">{item.valor}</td>
+                </tr>
+              ))}
+              {!isEven && (
+                <tr
+                  className={segundaMetade.length % 2 === 0 ? 'bg-[#FEFEFE] flex justify-between flex-grow items-center' : 'bg-[#FCD3B7] flex justify-between flex-grow items-center'}
+                >
+                  <td className="px-4 py-2 text-xl font-bold">XXXXXXXXXXXXXXXXXXXXXX</td>
+                  <td className="px-4 py-2 w-full max-w-32 text-xl font-bold">XXXXXX</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -8,13 +8,12 @@ import LogoUllian from "/public/assets/logo-ullian.png"
 export default async function HomeLayout() {
 
   const data = await getProducts();
-  console.log(data);
 
   return (
-    <section className="w-full min-h-screen bg-[#F58635]">
+    <section className="w-full min-h-screen bg-[#F58635] px-8">
       <div className=" relativew-full flex justify-center py-4 items-center">
         <h1 className="text-7xl text-white font-bold">CONGELADOS</h1>
-        <div className="absolute top-4 left-4 overflow-hidden max-w-[18.75rem]">
+        <div className="absolute top-4 left-8 overflow-hidden max-w-[18.75rem]">
           <Image
             alt="Ullian Logo"
             src={LogoUllian}
